2c35244f1c8dd3f046da7fc0d772696dd9a55a55
guptasonu14 <sguptabudha@gmail.com>
2024-04-21 01:22:02 +0530

    project complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import './App.css';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Collapse, IconButton } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 import React, { useState, useEffect } from 'react';
-
 import { KeyboardArrowDown, KeyboardArrowUp } from '@mui/icons-material';
-
-import dayjs from 'dayjs';
 import axios from 'axios';
-
 
 const App = () => {
   const [holdings, setHoldings] = useState([]);
+  const [expandedGroups, setExpandedGroups] = useState({});
 
   useEffect(() => {
     // Fetch holdings data from API
@@ -22,11 +19,10 @@ const App = () => {
       });
   }, []);
 
-  const renderRow = (holding) => (
-    <TableRow key={holding.ticker}>
+  const renderRow = (holding, index) => (
+    <TableRow key={holding.ticker} className={`colorClasses ${index % 2 === 0 ? '' : 'odd'}`}>
       <TableCell>{holding.name}</TableCell>
       <TableCell>{holding.ticker}</TableCell>
-      <TableCell>{holding.asset_class}</TableCell>
       <TableCell>{holding.avg_price}</TableCell>
       <TableCell>{holding.market_price}</TableCell>
       <TableCell>{holding.latest_chg_pct}</TableCell>
@@ -34,8 +30,15 @@ const App = () => {
     </TableRow>
   );
 
+  const toggleGroup = (assetClass) => {
+    setExpandedGroups(prevState => ({
+      ...prevState,
+      [assetClass]: !prevState[assetClass] 
+    }));
+  };
+
   const renderGroupedRows = () => {
-    // Group holdings by asset_class
+   
     const groupedHoldings = holdings.reduce((acc, holding) => {
       acc[holding.asset_class] = [...(acc[holding.asset_class] || []), holding];
       return acc;
@@ -45,32 +48,40 @@ const App = () => {
       <React.Fragment key={assetClass}>
         <TableRow className="groupHeader" onClick={() => toggleGroup(assetClass)}>
           <TableCell colSpan={7}>
-            {assetClass} {/* Display asset class as group header */}
+            {expandedGroups[assetClass] ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
+            {assetClass} ({groupedHoldings[assetClass].length})
           </TableCell>
         </TableRow>
-        {groupedHoldings[assetClass].map(holding => renderRow(holding))}
+        {expandedGroups[assetClass] &&
+          <React.Fragment>
+            <TableRow>
+              <TableCell colSpan={7}> 
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>NAME OF THE HOLDING</TableCell>
+                      <TableCell>TICKER</TableCell>
+                      <TableCell>AVERAGE PRICE</TableCell>
+                      <TableCell>MARKET PRICE</TableCell>
+                      <TableCell>LATEST CHANGE PERCENTAGE</TableCell>
+                      <TableCell>MARKET VALUE IN BASE CCY</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {groupedHoldings[assetClass].map((holding, index) => renderRow(holding, index))}
+                  </TableBody>
+                </Table>
+              </TableCell>
+            </TableRow>
+          </React.Fragment>
+        }
       </React.Fragment>
     ));
   };
 
-  const toggleGroup = (assetClass) => {
-    // Implement logic to toggle group expansion/collapse if desired
-  };
-
   return (
-    <TableContainer component={Paper}>
+    <TableContainer component={Paper} className="paperContainer">
       <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell>Ticker</TableCell>
-            <TableCell>Asset Class</TableCell>
-            <TableCell>Average Price</TableCell>
-            <TableCell>Market Price</TableCell>
-            <TableCell>Latest Change Percentage</TableCell>
-            <TableCell>Market Value (Base CCY)</TableCell>
-          </TableRow>
-        </TableHead>
         <TableBody>
           {holdings.length > 0 ? renderGroupedRows() : <TableRow><TableCell colSpan={7}>Loading...</TableCell></TableRow>}
         </TableBody>
